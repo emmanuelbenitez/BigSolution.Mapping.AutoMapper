94bec2167670d8a650d813b3a01979e192b0de88
Emmanuel Benitez <eben@aprico-consult.com>
2020-08-26 17:02:05 +0200

    Add resources submodule, update to latest version of R# settings, update to Validation 2.0, use pipeline templates

--- src/Mapping.AutoMapper.Tests/AutoMapperMapperFixture.cs
+++ src/Mapping.AutoMapper.Tests/AutoMapperMapperFixture.cs
@@ -1,3 +1,21 @@
+#region Copyright & License
+
+// Copyright © 2020 - 2020 Emmanuel Benitez
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
 using System;
 using System.Diagnostics.CodeAnalysis;
 using AutoMapper;
@@ -11,20 +29,19 @@ namespace BigSolution.Infra.Mapping
     {
         [Fact]
         [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+        public void CreateFailed()
+        {
+            Action action = () => new AutoMapperMapper<Source, Destination>(null);
+            action.Should().ThrowExactly<ArgumentNullException>().Where(exception => exception.ParamName == "mapper");
+        }
+
+        [Fact]
+        [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
         public void CreateSucceeds()
         {
             var fakeMapper = new Mock<IMapper>();
             Action action = () => new AutoMapperMapper<Source, Destination>(fakeMapper.Object);
             action.Should().NotThrow();
-
-        }
-
-        [Fact]
-        [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
-        public void CreateFailed()
-        {
-            Action action = () => new AutoMapperMapper<Source, Destination>(null);
-            action.Should().ThrowExactly<ArgumentNullException>().Where(exception => exception.ParamName == "mapper");
         }
 
         [Fact]
@@ -34,7 +51,7 @@ namespace BigSolution.Infra.Mapping
             fakeMapper.Setup(m => m.Map<Destination>(It.IsAny<Source>()))
                 .Returns(new Destination());
             var mapper = new AutoMapperMapper<Source, Destination>(fakeMapper.Object);
-            
+
             mapper.Map(new Source()).Should().NotBeNull();
             fakeMapper.Verify(m => m.Map<Destination>(It.IsAny<Source>()), Times.Once);
         }
